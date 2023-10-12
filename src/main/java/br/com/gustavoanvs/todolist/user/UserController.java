820ae93d807facb1dfaebc475601549231b29b6c
Gustavo <gusttavo.developer@gmail.com>
2023-10-12 03:04:43 -0300

    Spring boot initializr

---
+++ src/main/java/br/com/gustavoanvs/todolist/user/UserController.java
@@ -0,0 +1,26 @@
+package br.com.gustavoanvs.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /**
+     * String
+     * Integer
+     * Double
+     * FLoat
+     * Char
+     * Date
+     * void sem retorno
+     */
+
+     @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.println(userModel.getUsername());
+    }
+}   
